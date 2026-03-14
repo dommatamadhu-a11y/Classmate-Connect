ae83b3f45dd44e350dad507fbe999ea9d7058a01
dommatamadhu-a11y <dommatamadhu@gmail.com>
2026-03-14 22:09:56 +0530

    Refactor Firebase configuration and functions

--- app.js
+++ app.js
@@ -21,27 +21,24 @@ onSnapshot
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
-const firebaseConfig = {
-
+const firebaseConfig={
 apiKey:"AIzaSyAK01_ZKFoPQQrpFqoRnlvuH0iVXLF7mqA",
 authDomain:"classmate-connect-4ef14.firebaseapp.com",
 projectId:"classmate-connect-4ef14",
 storageBucket:"classmate-connect-4ef14.appspot.com",
 messagingSenderId:"836999548178",
 appId:"1:836999548178:web:8fc82fcf07289647c5f7cb"
-
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const provider = new GoogleAuthProvider();
+const app=initializeApp(firebaseConfig);
+const auth=getAuth(app);
+const db=getFirestore(app);
+const provider=new GoogleAuthProvider();
 
 let currentUser;
 let chatUser;
+let profileUser;
 
-
-// LOGIN
 
 window.googleLogin=function(){
 
@@ -51,12 +48,6 @@ signInWithPopup(auth,provider)
 
 currentUser=result.user;
 
-await setDoc(doc(db,"users",currentUser.uid),{
-
-online:true
-
-},{merge:true});
-
 document.getElementById("login").style.display="none";
 document.getElementById("profile").style.display="block";
 
@@ -64,9 +55,6 @@ document.getElementById("profile").style.display="block";
 
 };
 
-
-
-// SAVE PROFILE
 
 window.saveProfile=async function(){
 
@@ -90,9 +78,6 @@ showSection("find");
 };
 
 
-
-// FIND USERS
-
 window.findUsers=async function(){
 
 let school=document.getElementById("searchSchool").value.toLowerCase();
@@ -112,7 +97,7 @@ let letter=d.name.charAt(0).toUpperCase();
 
 html+=`
 
-<div class="card">
+<div class="card" onclick="viewProfile('${docu.id}')">
 
 <div class="row">
 
@@ -142,8 +127,37 @@ document.getElementById("results").innerHTML=html;
 };
 
 
+window.viewProfile=async function(id){
 
-// FRIEND REQUEST
+profileUser=id;
+
+let docu=await getDoc(doc(db,"users",id));
+let d=docu.data();
+
+document.getElementById("pName").innerText=d.name;
+document.getElementById("pSchool").innerText="School: "+d.school;
+document.getElementById("pYear").innerText="Year: "+d.year;
+document.getElementById("pCity").innerText="City: "+d.city;
+document.getElementById("pStatus").innerText=d.online?"Online":"Offline";
+
+showSection("profileView");
+
+};
+
+
+window.startChat=function(){
+
+chatUser=profileUser;
+
+document.getElementById("chatName").innerText=
+document.getElementById("pName").innerText;
+
+showSection("chatScreen");
+
+loadMessages();
+
+};
+
 
 window.sendRequest=async function(id){
 
@@ -160,161 +174,6 @@ alert("Request sent");
 
 };
 
-
-
-// LOAD REQUESTS
-
-window.loadRequests=function(){
-
-const q=query(
-collection(db,"friendRequests"),
-where("to","==",currentUser.uid),
-where("status","==","pending")
-);
-
-onSnapshot(q,(snapshot)=>{
-
-let html="";
-
-snapshot.forEach(docu=>{
-
-let d=docu.data();
-
-html+=`
-
-<div class="card">
-
-${d.from}
-
-<button onclick="acceptRequest('${docu.id}','${d.from}')">
-Accept
-</button>
-
-</div>
-
-`;
-
-});
-
-document.getElementById("requestList").innerHTML=html;
-
-});
-
-};
-
-
-
-// ACCEPT REQUEST
-
-window.acceptRequest=async function(id,from){
-
-await addDoc(collection(db,"friends"),{
-
-user1:currentUser.uid,
-user2:from
-
-});
-
-await updateDoc(doc(db,"friendRequests",id),{
-
-status:"accepted"
-
-});
-
-};
-
-
-
-// CHAT LIST
-
-window.loadChats=function(){
-
-onSnapshot(collection(db,"messages"),async(snapshot)=>{
-
-let chats={};
-
-snapshot.forEach(docu=>{
-
-let m=docu.data();
-
-if(m.from===currentUser.uid || m.to===currentUser.uid){
-
-let friend=m.from===currentUser.uid?m.to:m.from;
-
-if(!chats[friend] || chats[friend].time<m.time){
-
-chats[friend]=m;
-
-}
-
-}
-
-});
-
-let html="";
-
-for(let id in chats){
-
-let userDoc=await getDoc(doc(db,"users",id));
-
-let user=userDoc.data();
-
-let letter=user.name.charAt(0).toUpperCase();
-
-let last=chats[id].text;
-
-let time=new Date(chats[id].time).toLocaleTimeString();
-
-html+=`
-
-<div class="card" onclick="openChat('${id}')">
-
-<div class="row">
-
-<div class="avatar">${letter}</div>
-
-<div>
-
-<div>${user.name}</div>
-<div style="font-size:12px;color:gray">${last}</div>
-
-</div>
-
-</div>
-
-<div style="font-size:11px;color:gray">${time}</div>
-
-</div>
-
-`;
-
-}
-
-document.getElementById("chatList").innerHTML=html;
-
-});
-
-};
-
-
-
-// OPEN CHAT
-
-window.openChat=function(id){
-
-chatUser=id;
-
-document.getElementById("chatName").innerText=id;
-
-showSection("chatScreen");
-
-loadMessages();
-
-};
-
-
-
-// SEND MESSAGE
 
 window.sendMsg=async function(){
 
@@ -333,9 +192,6 @@ document.getElementById("msgInput").value="";
 
 };
 
-
-
-// LOAD MESSAGES
 
 function loadMessages(){
 
@@ -381,30 +237,10 @@ document.getElementById("chatBox").innerHTML=html;
 }
 
 
-
-// TYPING
-
-window.typing=async function(){
-
-await setDoc(doc(db,"typing",chatUser),{
-
-user:currentUser.uid
-
-});
-
-};
-
-
-
-// SHOW SECTION
-
 window.showSection=function(id){
 
 document.querySelectorAll(".section").forEach(s=>s.style.display="none");
 
 document.getElementById(id).style.display="block";
-
-if(id==="requests") loadRequests();
-if(id==="chats") loadChats();
 
 };
